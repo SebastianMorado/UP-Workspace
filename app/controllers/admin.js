e3afcd869f073f8ff1b28a19ff112133d4dccbc6
alwindeleon <bludeleon@gmail.com>
2018-02-02 13:22:57 +0800

    edit amin

--- app/controllers/admin.js
+++ app/controllers/admin.js
@@ -10,7 +10,7 @@ module.exports = function (app) {
 router.get('/', function (req, res, next) {
   Article.find(function (err, articles) {
     if (err) return next(err);
-    res.render('index', {
+    res.render('home', {
       title: 'Generator-Express MVC',
       articles: articles
     });
